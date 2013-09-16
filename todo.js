ec9769a3f8b1c326c3f4c4839b70a8f32d5eae5f
MakerSquare Student <students@themakersquare.com>
2013-09-16 11:44:02 -0500

    Add template, submit trigger

--- todo.js
+++ todo.js
@@ -1,4 +1,32 @@
 $(document).ready(function(){
+	var html = $("#template .box").html();
+	var template = _.template(html);
+
 	var tasks = [
-	]
+	{
+		name: "Create TODO list",
+		complete: false
+	},
+	{
+		name: "Implement Exercise",
+		complete: false
+	}
+	];
+
+	for(var i = 0; i < tasks.length; i++){
+		var newHtml = template(tasks[i]);
+		$(".display").append(newHtml);
+		console.log(newHtml);
+	}
+
+	$(".subAdd").on("click", function(){
+		var newTask = {
+			name: $(".taskName").val(),
+			complete: false
+		}
+		$(".taskName").val("");
+		newHtml = template(newTask);
+		$(".display").append(newHtml);
+	});
+
 });
